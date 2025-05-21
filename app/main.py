53015ddfa548dbded70c841c73fd8144f122f5de
Maksym Buslenko <maksbusl@gmail.com>
2025-05-21 20:16:46 +0300

    add wife/husband in init

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int) -> None:
+    def __init__(self,
+                 name: str,
+                 age: int,
+                 ) -> None:
         self.name = name
         self.age = age
+        self.wife = None
+        self.husband = None
         Person.people.update({self.name: self})
 
 
